c76b359bfa03476f0b5da038d4cdc3438adbe89e
oncefirefly <92741839+oncefirefly@users.noreply.github.com>
2021-12-13 19:30:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
   let currentFloor = 2;
+  let currentFlat = 1;
   let floorPath = $(".home-image path");
   let counterUp = $(".counter-up");
   let counterDown = $(".counter-down");
+  let modal = $(".modal");
+  let modalCloseButton = $(".modal-close-button");
+  let viewFlatsButton = $(".view-flats");
+  let flatPath = $(".flats path");
+  let flatLink = $(".flat-link");
 
   floorPath.on("mouseover", function () {
     floorPath.removeClass("current-floor");
@@ -13,14 +19,14 @@ $(document).ready(function () {
     $(`[data-floor = ${currentFloor}]`).toggleClass("current-floor");
   });
 
-//   floorPath.on("click", function () {
-//     floorPath.removeClass("current-floor");
+  //   floorPath.on("click", function () {
+  //     floorPath.removeClass("current-floor");
 
-//     currentFloor = $(this).attr("data-floor");
+  //     currentFloor = $(this).attr("data-floor");
 
-//     $(".counter").text(currentFloor);
-//     $(`[data-floor = ${currentFloor}]`).toggleClass("current-floor");
-//   });
+  //     $(".counter").text(currentFloor);
+  //     $(`[data-floor = ${currentFloor}]`).toggleClass("current-floor");
+  //   });
 
   counterUp.on("click", function () {
     let usCurrentFloor;
@@ -55,4 +61,40 @@ $(document).ready(function () {
       $(`[data-floor = ${usCurrentFloor}]`).toggleClass("current-floor");
     }
   });
+
+  viewFlatsButton.on("click", toggleModal);
+
+  floorPath.on("click", toggleModal);
+
+  modalCloseButton.on("click", toggleModal);
+
+  flatPath.on("mouseover", function () {
+    let currentFlat = $(this).attr("data-flat") - 1;
+
+    $(`.flat-link:eq(${currentFlat})`).toggleClass("current-flat-link");
+  });
+
+  flatPath.on("mouseout", function () {
+    $(".flat-link").removeClass("current-flat-link");
+  });
+
+  flatLink.on("mouseover", function () {
+    let currentFlat = +$(this).attr("data-flat-link");
+    // console.log(currentFlat);
+
+    let usCurrentFlat = currentFlat.toLocaleString("en-Us", {
+      minimumIntegerDigits: 2,
+      useGroupping: false,
+    });
+
+    $(`[data-flat = ${usCurrentFlat}]`).addClass("current-flat-path");
+  });
+
+  flatLink.on("mouseout", function () {
+    $(".flats path").removeClass("current-flat-path");
+  })
+
+  function toggleModal() {
+    modal.toggleClass("is-open");
+  }
 });
